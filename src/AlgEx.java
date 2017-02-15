01ba564cc715d487108f10571b45dc92982ddcaa
TTOJlTOPA <zhuk.valeriy.98@gmail.com>
2017-02-15 20:47:35 +0300

    rewrite

--- src/AlgEx.java
+++ src/AlgEx.java
@@ -5,30 +5,17 @@ import java.util.*;
 public class AlgEx {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(new File("input.txt"));
-        List<Integer> treeDeg = new ArrayList<>();
-        String bin = Long.toString(scanner.nextLong(), 2);
-        int size = bin.length();
-        for(int i = 0; i < size; i++){
-            if(bin.charAt(i) == '1'){
-                treeDeg.add(size - i - 1);
-            }
-        }
-        Collections.reverse(treeDeg);
-        print(treeDeg);
-        scanner.close();
-    }
-
-    private static void print(List<Integer> result) throws Exception {
         FileWriter writer = new FileWriter("output.txt");
-        Iterator<Integer> iter;
-        if (result.size() != 0) {
-            iter = result.iterator();
-            while (iter.hasNext()) {
-                writer.write(iter.next() + "\n");
+        long num = scanner.nextLong();
+        int counter = 0;
+        while (num > 0) {
+            if(num % 2 != 0) {
+                writer.write(counter + "\n");
             }
-        } else {
-            writer.write(-1 + "");
+            num /= 2;
+            counter++;
         }
         writer.close();
+        scanner.close();
     }
 }
